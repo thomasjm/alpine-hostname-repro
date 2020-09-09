443c5da621c98aef2be42b71b89269a003b5bd6f
Tom McLaughlin <tom@codedown.io>
2020-09-08 22:31:52 -0600

    Rename

--- default.nix
+++ default.nix
@@ -1,11 +1,7 @@
 {
   haskellNix ? import (builtins.fetchTarball "https://github.com/input-output-hk/haskell.nix/archive/e328d1de1f2fd5319d39c825851cdc9cf8158bca.tar.gz") {}
 
-, nixpkgsSrc ? builtins.fetchGit {
-    url = "https://github.com/NixOS/nixpkgs.git";
-    rev = "9ea61f7bc4454734ffbff73c9b6173420fe3147b";
-    ref = "release-20.03";
-  }
+, nixpkgsSrc ? import ./pinned-nixpkgs.nix
 
 , nixpkgsArgs ? haskellNix.nixpkgsArgs
 
@@ -14,6 +10,18 @@
 
 with pkgs;
 
-{
-  app = import ./app.nix { inherit pkgs; };
+let src = ./src;
+
+in
+
+haskell-nix.stackProject {
+  inherit src;
+
+  # modules = [
+  #   {
+  #     # Necessary to prevent gcc runtime dependency blowing up the closure size; see
+  #     # https://github.com/input-output-hk/haskell.nix/issues/829
+  #     packages.http-client-alpine-repro.components.exes.http-client-alpine-repro-exe = false;
+  #   }
+  # ];
 }
